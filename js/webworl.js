15d54de0b2bf58bf347bf41298f15f1646ce1ba7
Artur da Silva Rezende <arturrezende4@gmail.com>
2020-02-28 12:28:13 -0300

    Alterações feitas na logo foram finalizadas

---
+++ js/webworl.js
@@ -0,0 +1,30 @@
+/**
+ * Projeto WebWolrd versão 1.1
+ * @author Artur da Silva Rezende
+ */
+
+//A estrutura abaixo faz a transição da frase da logo aparecer e desaparecer 
+
+var home = document.querySelector(".home"); //Essa variável esta recebendo a classe "home" do container <a>
+var home_message = document.querySelector(".home-message"); //Essa variável esta recebendo a classe "home-message" da tag <p>
+var logo_x = window.matchMedia("(max-width: 700px)"); //Essa variável controla a média query
+
+home.addEventListener("mouseenter", message_logo_enter);
+home.addEventListener("mouseleave", message_logo_leave);
+
+function message_logo_enter(){
+    home_message.style.opacity = "1";
+    home_message.style.transition = "opacity 1s";
+
+    if(logo_x.matches){
+        home_message.style.display = "none";
+    } 
+}
+
+function message_logo_leave(){
+    home_message.style.opacity = "0";
+    home_message.style.transition = "opacity 1s";
+}
+
+
+
